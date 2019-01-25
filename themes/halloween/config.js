76b5573ef730e3032db1b07d64f058714f68ea46
EGNEntertainment <chrispsgamer@gmail.com>
2019-01-25 04:40:58 -0500

    Update config.js

--- themes/halloween/config.js
+++ themes/halloween/config.js
@@ -2,8 +2,8 @@ window.cytubeEnhanced.getModule('themes').done(function (extraModules) {
     extraModules.add({
         title: 'Halloween',
         name: 'halloween',
-        cssUrl: 'https://rawgit.com/kaba99/cytube-enhanced/master/themes/halloween/theme.css',
+        cssUrl: 'https://raw.githack.com/EGNEntertainment/cytube-enhanced/master/themes/halloween/theme.css',
         jsUrl: 'https://raw.githack.com/EGNEntertainment/cytube-enhanced/master/themes/halloween/theme.js',
-        pictureUrl: 'https://rawgit.com/kaba99/cytube-enhanced/master/themes/halloween/screenshot.png'
+        pictureUrl: 'https://files.catbox.moe/d1uxbp.jpg'
     });
 });
